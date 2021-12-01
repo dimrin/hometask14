19732a7f3baa0f8671fac4c4de649f60b63c1244
dimrin <zuka537122@gmail.com>
2021-12-01 21:46:03 +0200

    Finished

--- src/com/company/dymrin14/Main.java
+++ src/com/company/dymrin14/Main.java
@@ -1,8 +1,48 @@
 package com.company.dymrin14;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        List<String> strings = new ArrayList<>();
+        strings.add("Dog");
+        strings.add("Cat");
+        strings.add("Dog");
+        strings.add("Bird");
+        strings.add("Fish");
+        strings.add("Fish");
+        System.out.println(convertToUnique(strings));
+
+
+        List<Integer> values = new ArrayList<>();
+        values.add(3);
+        values.add(2);
+        values.add(4);
+        values.add(3);
+        values.add(1);
+        System.out.println(getSortedUniqueIntegersASC(values));
+        System.out.println(getSortedUniqueIntegersDESC(values));
+
     }
+
+
+    static Set<String> convertToUnique(List<String> strings) {
+        return new LinkedHashSet<>(strings);
+    }
+
+
+    static Set<Integer> getSortedUniqueIntegersASC(List<Integer> values) {
+        return new TreeSet<>(values);
+    }
+
+    static Set<Integer> getSortedUniqueIntegersDESC(List<Integer> values) {
+        Set<Integer> reverseUniqueInteger = new TreeSet<>(Comparator.reverseOrder());
+        reverseUniqueInteger.addAll(values);
+        return reverseUniqueInteger;
+
+    }
+
+
 }
